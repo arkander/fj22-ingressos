528a445768bd61c3add28e6850ccb612719010bc
Adauto Fabiano e Silva <lab6936@caelum113-09>
2017-08-12 10:27:22 -0300

    ingressos

---
+++ src/main/java/br/com/caelum/helpers/TipoDeIngresso.java
@@ -0,0 +1,33 @@
+package br.com.caelum.helpers;
+
+import java.math.BigDecimal;
+
+import br.com.caelum.ingresso.model.Desconto;
+import br.com.caelum.ingresso.model.DescontoEstudante;
+import br.com.caelum.ingresso.model.DescontoTrintaPorCento;
+import br.com.caelum.ingresso.model.SemDesconto;
+
+public enum TipoDeIngresso {
+	
+	INTEIRO(new SemDesconto()),
+	ESTUDANTE(new DescontoEstudante()),
+	BANCO(new DescontoTrintaPorCento());
+	
+	private Desconto desconto;
+	
+	TipoDeIngresso(Desconto desconto){
+		this.desconto = desconto;
+	}
+	
+	public BigDecimal aplicaDesconto(BigDecimal valor){
+		return desconto.aplicarDescontoSobre(valor);
+		
+	}
+	
+	public String getDescricao(){
+		return desconto.getDescricao();
+		
+	}
+	
+
+}
